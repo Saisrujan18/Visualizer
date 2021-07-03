2b13071b65516a743c72cea81577738c13438ab0
Saisrujan18 <yayisaisrujan@gmail.com>
2021-07-03 21:53:39 +0530

    2/3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import way from './way.PNG';
 import noway from './wall.jpg';
 import stop from './ss.PNG';
 
+// import Node from './Node/Node';
+import {dijkstra, getNodesInShortestPathOrder} from './Components/Dijkstra.js';
+
+
+// import Node from './Components/Node';
 
 class App extends React.Component 
 {
@@ -113,15 +118,18 @@ class App extends React.Component
 		};
 		this.handleClick=this.handleClick.bind(this);
 		this.clearg=this.clearg.bind(this);
-		// this.temp=this.temp.bind(this);
 		this.DFSI=this.DFSI.bind(this);
 		this.DFS=this.DFS.bind(this);
 		this.Innerwhile=this.Innerwhile.bind(this);
+		this.animatedfs=this.animatedfs.bind(this);
+		this.BFS=this.BFS.bind(this);
+		this.Binnerwhile=this.Binnerwhile.bind(this);
+
+		// this.DIJKSTRA=this.DIJKSTRA.bind(this);
 	}
 
 	handleClick(event)
 	{
-		// console.log(this.state);
 		if(this.state.Busy===true)return;
 		const where=event.target.id;
 		
@@ -135,7 +143,7 @@ class App extends React.Component
 		if(temp[r][c]===1){temp[r][c]=0;}
 		else {temp[r][c]=1;}
 		this.setState({Grid:temp});
-		// console.log(this.state.Grid);
+	
 		if(event.target.src===noway){event.target.src=way;}
 		else{event.target.src=noway;}
 	}
@@ -223,57 +231,11 @@ class App extends React.Component
 		this.DFS();	
 		this.setState({Busy:false});
 	}
-	//  DFS(i,j)
-	// {
-	// 	// console.log(Grid);
-	// 	// setTimeout( ()=>
-	// 	// {
-			
-	// 		if(i<0 || i>14 || j<0 || j>29)return;
-	
-	// 		if(this.state.Grid[i][j]===3)
-	// 		{
-	// 			document.getElementById(0).src=stop;
-	// 			return;
-	// 		}
-			
-	// 		if(this.state.Grid[i][j]===0 || this.state.Grid[i][j]===2)
-	// 		{
-				
-	// 			if(this.state.Grid[i][j]!==2)
-	// 			{
-	// 				document.getElementById(i*1000+j).src=visited;
-	// 			}
-	// 			var g=[];
-	// 			for(var itr=0;itr<15;itr++)
-	// 			{
-	// 				var temp=[];
-	// 				for(var it=0;it<30;it++)
-	// 				{
-	// 					temp.push(this.state.Grid[itr][it]);
-	// 				}
-	// 				g.push(temp);
-	// 			}
-	// 			g[i][j]=10;
-	// 			// console.log(g);
-	// 			this.setState({Grid:g});
-
-	// 			this.DFS(i,j+1);
-	// 			// this.DFS(i+1,j);
-	// 			// this.DFS(i-1,j);
-	// 			// this.DFS(i,j-1);
-	// 		}
-	// 	// },
-	// 	// 90);
-	// }
-
-	Innerwhile()
+	Innerwhile(sstack,nodes,short)
 	{
-		var stack=this.state.dfstack;
-		console.log(stack);
-		if(stack.length===0){clearInterval(this.state.dfstime);return;}
+		var stack=sstack;
 
-		var s= stack[stack.length-1];
+		var s = stack[stack.length-1];
 		stack.pop();
 		
 		var r=(s-s%1000)/1000;
@@ -281,88 +243,284 @@ class App extends React.Component
 		
 		var val=this.state.Grid[r][c];
 
-		if(val===3)
-		{
-			// done=true;
-			// returns.push(stack);
-			// returns.push(done);
-			// return returns;
-			this.setState({dfstack:[]});
-			clearInterval(this.state.dfstime);
-			// return []; 
-		}
-		if(val===10 || val===1)
-		{
-			// returns.push(stack);
-			// returns.push(done);
-			// return returns;
-			this.setState({dfstack:stack});
-			// return stack;
-		}
+		if(val===3){return [[],nodes,short]; }
+		if(val===10 || val===1){return [stack,nodes,short];}
 
 		let temp=this.state.Grid;
 		temp[r][c]=10;
+		nodes.push(r*1000+c);
 		this.setState({Grid:temp});
-		document.getElementById(r*1000+c).src=visited;
 
-		if(r<14){stack.push(r*1000+c+1000);}
-		if(c<29){stack.push(r*1000+c+1);}
-		// returns.push(done);
-		this.setState({dfstack:stack});
-		// return stack;
+		if(r<14){stack.push(r*1000+c+1000);short[r+1][c]=r*1000+c;}
+		if(c<29){stack.push(r*1000+c+1);short[r][c+1]=r*1000+c;}
+		return [stack,nodes,short];
+	}
+
+	animatedfs(nodes,short)
+	{
+		for(let i=0;i<=nodes.length;i++)
+		{
+			if(i===nodes.length)
+			{
+				setTimeout(() =>{this.animateShort(short);}, 20*i);
+				return;
+			}
+			setTimeout(() => 
+			{
+				const node = nodes[i];
+				if(node!==0)document.getElementById(node).src=visited;
+			  }, 20 * i);
+		}
+	}
+
+	animateShort(short)
+	{
+		for (let i = 0; i < short.length; i++) 
+		{
+			setTimeout(() => {
+			  const node = short[i];
+			  document.getElementById(node).src=start;
+			}, 100*i);
+		}
 	}
 
 	DFS()
 	{
 		var stack =[];
 		stack.push(0);
-		this.setState({dfstack:stack});
-		var myintr=setInterval(this.Innerwhile(),1);
-		this.setState({dfstime:myintr});
-		// setInterval(this.Innerwhile(),90);
-		// var intr = setInterval(this.Innerwhile(), 900);
-
-		// stack=this.state.dfstack;
-		// if(stack.length===0)clearInterval(intr);
-		// (async () => 
-		// {
-		// 	var i=0;
-		// 	while (await new Promise(resolve => setTimeout(() => {console.log("mf");}, 10)) < 1) 
-		// 	{
-		// 		this.Innerwhile();
-		// 		stack=this.state.dfstack;
-		// 		if(stack.length===0)break;
-		// 	}
-		// })();
-		// while(stack.length!==0)
-		// {
-			// this.setState({dfstack:stack});
-			// console.log(this.dfstack);
-			// setTimeout((stack)=>{stack=this.Innerwhile(stack)},90);
-			// stack=this.Innerwhile(stack);
-			// stack=this.state.dfstack;
-			// console.log(stack);
-
-			// var s= stack[stack.length-1];
-			// stack.pop();
-			// r=(s-s%1000)/1000;
-			// c=s%1000;
-			// var val=this.state.Grid[r][c];
-
-			// if(val===3){break;}
-			// if(val===10){continue;}
-			// if(val===1){continue;}
-
-			// let temp=this.state.Grid;
-			// temp[r][c]=10;
-			// this.setState({Grid:temp});
-			// document.getElementById(r*1000+c).src=visited;
-
-			// if(r<14){stack.push(r*1000+c+1000);}
-			// if(c<29){stack.push(r*1000+c+1);}
-		// }
+		var answer=[];
+		var short=[];
+		for(let i=0;i<15;i++)
+		{
+			var t=[];
+			for(let j=0;j<30;j++)
+			{
+				t.push(-1);
+			}
+			short.push(t);
+		}
+		while(stack.length!==0)
+		{
+			var temp=this.Innerwhile(stack,answer,short);
+			stack=temp[0];
+			answer=temp[1];
+			short=temp[2];
+		}
+		// console.log(short);
+		var inorder=[];
+		if(short[14][29]===-1)
+		{
+			this.animatedfs(answer,inorder);
+		}
+		else
+		{
+			var r=14;
+			var c=29;
+			while(short[r][c]!==0)
+			{
+				inorder.push(short[r][c]);
+				var val=short[r][c];
+				r=(val-val%1000)/1000;
+				c=val%1000;
+			}
+			inorder.push(0);
+			this.animatedfs(answer,inorder);
+		}
 	}
 
+	Binnerwhile(q,nodes,short)
+	{
+		var queue=q;
+
+		var s = queue[queue.length-1];
+		queue.pop();
+		
+		var r=(s-s%1000)/1000;
+		var c=s%1000;
+		
+		var val=this.state.Grid[r][c];
+
+		if(val===3){return [[],nodes,short]; }
+		if(val===10 || val===1){return [queue,nodes,short];}
+
+		let temp=this.state.Grid;
+		temp[r][c]=10;
+		nodes.push(r*1000+c);
+		this.setState({Grid:temp});
+
+		if(r<14){queue.unshift(r*1000+c+1000);short[r+1][c]=r*1000+c;}
+		if(c<29){queue.unshift(r*1000+c+1);short[r][c+1]=r*1000+c;}
+		return [queue,nodes,short];
+	}
+	BFS()
+	{
+		var queue=[];
+		queue.push(0);
+		var answer=[];
+		var short=[];
+
+		for(let i=0;i<15;i++)
+		{
+			var t=[];
+			for(let j=0;j<30;j++)
+			{
+				t.push(-1);
+			}
+			short.push(t);
+		}
+		
+		while(queue.length!==0)
+		{
+			var temp=this.Binnerwhile(queue,answer,short);
+			queue=temp[0];
+			answer=temp[1];
+			short=temp[2];
+		}
+		var inorder=[];
+		if(short[14][29]===-1)
+		{
+			this.animatedfs(answer,inorder);
+		}
+		else
+		{
+			var r=14;
+			var c=29;
+			while(short[r][c]!==0)
+			{
+				inorder.push(short[r][c]);
+				var val=short[r][c];
+				r=(val-val%1000)/1000;
+				c=val%1000;
+			}
+			inorder.push(0);
+			this.animatedfs(answer,inorder);
+		}
+	}
+	// node(r,c,val,distance,prev)
+	// {
+	// 	return {r,c,val,distance,prev};
+	// 	// this.r=r;
+	// 	// this.c=c;
+	// 	// this.val=val;
+	// 	// this.distance=dist;
+	// 	// this.prev=prev;
+	// }
+	// DIJKSTRA() 
+	// {
+	// 	var visitedNodesInOrder = [];
+	// 	var unvisitedNodes = this.getAllNodes(this.state.Grid);
+	// 	// console.log(unvisitedNodes);
+	// 	unvisitedNodes[0].distance=0;
+	// 	console.log(unvisitedNodes);
+	// 	for(var i=0;i<15*30;i++)
+	// 	{
+	// 		if(unvisitedNodes[i].distance===0)
+	// 		{
+	// 			console.log(i);
+	// 		}
+	// 	}	
+	// 	var grid=this.gridform(this.state.Grid);
+	// 	grid[0][0].distance=0;
+
+	// 	while (!!unvisitedNodes.length) 
+	// 	{
+	// 		this.sortNodesByDistance(unvisitedNodes);
+	// 		console.log(unvisitedNodes);
+	// 		const closestNode = unvisitedNodes.shift();
+			
+	// 		if (closestNode.val===3)break;
+
+	// 		if (closestNode.val===1)continue;
+	// 		if (closestNode.distance===Infinity)break;
+			
+	// 		var temp=this.state.Grid;
+	// 		var r=closestNode.r;
+	// 		var c=closestNode.c;
+	// 		temp[r][c]=10;
+	// 		this.setState({Grid:temp});
+			
+	// 		closestNode.val=10;
+	// 		visitedNodesInOrder.push(closestNode);
+	// 		this.updateUnvisitedNeighbors(closestNode, grid);
+	// 	}
+	// 	var answer=[];
+	// 	for(var i=0;i<visitedNodesInOrder.length;i++)
+	// 	{
+	// 		answer.push(visitedNodesInOrder[i].r*1000+visitedNodesInOrder[i].c);
+	// 	}
+	// 	console.log(visitedNodesInOrder);
+
+
+	// 	this.animatedfs(answer,[]);
+	// }
+	// sortNodesByDistance(unvisitedNodes) 
+	// {
+	// 	unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+	// }
+	// updateUnvisitedNeighbors(node, grid) 
+	// {
+	// 	const unvisitedNeighbors = this.getUnvisitedNeighbors(node, grid);
+	// 	for (const neighbor of unvisitedNeighbors) 
+	// 	{
+	//  		neighbor.distance = node.distance + 1;
+	// 		neighbor.prev = node;
+	// 	}
+  	// }
+	// gridform(grid)
+	// {
+	// 	const nodes = [];
+	// 	for(var i=0;i<15;i++)
+	// 	{
+	// 		var temp=[];
+	// 		for(var j=0;j<30;j++)
+	// 		{
+	// 			var nod=this.node(i,j,grid[i][j],Infinity,null);
+	// 			// console.log(nod);
+	// 			// nodes.push(nod);
+	// 			temp.push(nod);
+	// 		}
+	// 		nodes.push(temp);
+	// 	}
+	// 	return nodes;
+	// }
+	// getUnvisitedNeighbors(node, grid) 
+	// {
+	// 	const neighbors = [];
+	// 	const {c, r} = node;
+	// 	if (r > 0) neighbors.push(grid[r - 1][c]);
+	// 	if (r < 14) neighbors.push(grid[r + 1][c]);
+	// 	if (c > 0) neighbors.push(grid[r][c - 1]);
+	// 	if (c < 29) neighbors.push(grid[r][c + 1]);
+	// 	// console.log(neighbors);
+	// 	return neighbors.filter(neighbor => (neighbor.val!==10));
+  	// }
+	// getAllNodes(grid) 
+	// {
+	// 	const nodes = [];
+	// 	for(var i=0;i<15;i++)
+	// 	{
+	// 		// var temp=[];
+	// 		for(var j=0;j<30;j++)
+	// 		{
+	// 			var nod=this.node(i,j,grid[i][j],Infinity,null);
+	// 			// console.log(nod);
+	// 			nodes.push(nod);
+	// 		}
+	// 		// nodes.push(temp);
+	// 	}
+	// 	return nodes;
+	// }
+	// getNodesInShortestPathOrder(grid) 
+	// {
+	// 	const nodesInShortestPathOrder = [];
+	// 	let currentNode = grid[14][29];
+	// 	while (currentNode !== null) 
+	// 	{
+	// 		nodesInShortestPathOrder.unshift(currentNode.r*1000+currentNode.c);
+	// 		currentNode = currentNode.prev;
+	// 	}
+	// 	return nodesInShortestPathOrder;
+  	// }
 	render()
 	{
 		return (
@@ -371,10 +529,10 @@ class App extends React.Component
 				{this.show()}
 
 				<div className="button">
-					<button className="buttons">Dijkstra</button>
-					<button className="buttons">BFS</button>
+					<button className="cl" onClick={this.clearg}>CLEAR</button>
+					<button className="buttons" onClick={this.DIJKSTRA}>DIJKSTRA</button>
+					<button className="buttons" onClick={this.BFS}>BFS</button>
 					<button className="buttons" onClick={this.DFSI}>DFS</button>
-					<button className="buttons" onClick={this.clearg}>Clear Grid</button>
 				</div>
 			
 			</div>
